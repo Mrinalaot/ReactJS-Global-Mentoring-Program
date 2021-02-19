175e3d686d972a983d32040e82d1ff4e0bb7cbdc
Mrinal Ghosh <Mrinal_Ghosh@epam.com>
2021-02-19 20:42:42 +0530

    [Task 2]: Webpack and Babel config added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+import React from 'react'
+
+
+const App = () => {
+
+    return (
+        <React.Fragment>
+            <p> Webpack finally working!!! </p>
+        </React.Fragment>
+    )
+}
+
+export default App
