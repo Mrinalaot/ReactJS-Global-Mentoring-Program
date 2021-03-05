4e1e7737587f497866d7a45fa7341eff5db4b63d
Mrinal Ghosh <Mrinal_Ghosh@epam.com>
2021-03-05 20:50:24 +0530

    [Task 4]: Add Edit and Delete a Movie feature

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import Menu from './Menu'
+import AddOrEditMovie from './AddOrEditMovie'
+import DeleteMovie from './DeleteMovie'
 
 const Item = ({ movie }) => {
+    const [mode, setMode] = useState('')
+
+    const onMovieEdit = () => setMode('edit')
+    const onMovieDelete = () => setMode('delete')
+    const onClose = () => setMode('')
+
     const genres = movie.genres.join(' & ')
 
     return (
         <div>
+            <Menu edit={onMovieEdit} delete={onMovieDelete} />
+            <AddOrEditMovie show={['add', 'edit'].includes(mode)} mode={mode} onClose={onClose}/>
+            <DeleteMovie show={['delete'].includes(mode)} mode={mode} onClose={onClose} />
             <img
                 src={movie.poster_path}
                 className="thumbnails"
